6c9cf5e36c2acbe0331d03f2fd9b7fd0a3c274f0
Kyle Sherman <ksherm337@gmail.com>
2016-09-22 01:54:07 -0400

    Added documentation to the UserInterface.cs class

--- assignment1/UserInterface.cs
+++ assignment1/UserInterface.cs
@@ -14,66 +14,48 @@ using System.Threading.Tasks;
 namespace assignment1
 {
     class UserInterface
-    {
-        private string errorCauseString;
-
+    {              
+        // this is the method that gets handles user input and returns it as a string
         public int GetUserInput()
         {
-            this.PrintMainMenu();
+            this.PrintMainMenu();                       // calls the PrintMainMenue method
 
-            string inputString = Console.ReadLine();
+            string inputString = Console.ReadLine();    // gets the users input
+
+            // while loop checking for propper user input,
+            // the user must enter a 1, 2, 3, 4, 5
+            // the input cannot be blank
             while (inputString != "1" && inputString != "2" && inputString != "3" && inputString != "4" && inputString != "5" && inputString != string.Empty && inputString != null)
             {
-                ErrorMessageHandling(errorCauseString = "badInput");
+                Console.Clear();
+                Console.WriteLine("That is not a valid option!");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+                                
+                Console.Clear();
 
-                this.PrintMainMenu();
-                inputString = Console.ReadLine();
+                this.PrintMainMenu();                               // print the main menu again
+                inputString = Console.ReadLine();                   // get the user input
             }
             Console.Clear();
+
+            // This try catch trys to parse the users input
+            // checks if the users input is propper
             try
             {
-                return Int32.Parse(inputString);
+                return Int32.Parse(inputString);                    // parse the input from the user
             }
-            catch(Exception EX)
+            catch(Exception EX)                                     // catch any exceptions thrown
             {
-                ErrorMessageHandling(errorCauseString = "noInput");
+                Console.Clear();
+                Console.WriteLine("Input cannot be empty and must be a number");
+                Console.WriteLine("Please try again.");
                 return 0;
             }
         }
 
-        private void ErrorMessageHandling(string errorCauseString)
-        {
-            if (errorCauseString == "badInput")
-            {
-                Console.Clear();
-                Console.WriteLine("That is not a valid option!");
-                Console.WriteLine("Please try again.");
-
-                System.Threading.Thread.Sleep(2500);
-                Console.Clear();
-            }
-
-            if (errorCauseString == "multiRead")
-            {
-                Console.Clear();
-                Console.WriteLine("The file can only be read once per run!");
-                Console.WriteLine("Please try again.");
-
-                System.Threading.Thread.Sleep(2500);
-                Console.Clear();
-            }
-
-            if (errorCauseString == "noInput")
-            {
-                Console.Clear();
-                Console.WriteLine("Input cannot be empty!");
-                Console.WriteLine("Please try again.");
-
-                System.Threading.Thread.Sleep(2500);
-                Console.Clear();
-            }
-        }
-
+        // this method holds the console.WriteLines for the main menu
+        // called by the method above
         private void PrintMainMenu()
         {
             Console.WriteLine("1) Load Wine List From CSV");
@@ -83,6 +65,8 @@ namespace assignment1
             Console.WriteLine("5) Exit");
         }
 
+        // this method is used to print the output result of the WineItemCollection array
+        // string is passed in from the wineItemCollection class when called
         public void PrintAllOutput(string allOutput)
         {
             Console.WriteLine(allOutput);
